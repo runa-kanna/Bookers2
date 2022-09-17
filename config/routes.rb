42aa8719eecf272251906b3afca16aea35ef8dff
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-09-17 08:26:40 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'homes#top'
+  get "homes/about" => "homes#about", as: "about"
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:show, :edit ]
+  patch 'books/:id' => 'books#update',as: 'update_book'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
