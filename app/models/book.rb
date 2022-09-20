7d50e55b6223ab9446cf851b1723c8feed782687
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-09-20 12:29:13 +0000

    Initial Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,15 +3,6 @@ class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   
-  
-  def get_image
-    if image.attached?
-      image
-    else
-      'no_image.jpg'
-    end
-  end
-  
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
-  resources :users, only: [:show, :edit, :update, :index]
   patch 'books/:id' => 'books#update',as: 'update_book'
 
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:show, :edit, :update, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
