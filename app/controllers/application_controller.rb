b4ffec41fb5ee4e0ebef299c5375767ba9e33e51
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-09-21 11:26:13 +0000

    Initial Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    books_path
+    user_path(@user.id)
   end
-  
+
   def after_sign_out_path_for(resource)
-    about_path
+    '/'
   end
-  
-  
+
+
   protected
 
   def configure_permitted_parameters
@@ -19,4 +19,3 @@ class ApplicationController < ActionController::Base
   end
 end
 
-  
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
-  get "homes/about" => "homes#about", as: "about"
+  get "home/about" => "homes#about", as: "about"
   patch 'books/:id' => 'books#update',as: 'update_book'
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
